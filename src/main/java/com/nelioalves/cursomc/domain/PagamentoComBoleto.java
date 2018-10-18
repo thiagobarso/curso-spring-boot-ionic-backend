6304f7c9634131a93ae835a57a539563eb94b4a6
Thiago Barbosa Soares <thiagobarso@gmail.com>
2018-10-18 16:13:41 -0300

    Primeiro Commit

---
+++ src/main/java/com/nelioalves/cursomc/domain/PagamentoComBoleto.java
@@ -0,0 +1,48 @@
+package com.nelioalves.cursomc.domain;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.nelioalves.cursomc.domain.enums.EstadoPagamento;
+
+@Entity
+public class PagamentoComBoleto extends Pagamento{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	private Date dataPagamento;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	private Date dataVencimento;
+	
+	public PagamentoComBoleto() {
+		super();
+	}
+
+	public PagamentoComBoleto(Integer id, EstadoPagamento estado, Pedido pedido, Date dataVencimento, Date dataPagamento) {
+		super(id, estado, pedido);
+		this.dataPagamento = dataPagamento;
+		this.dataVencimento = dataVencimento;
+	}
+
+	public Date getDataPagamento() {
+		return dataPagamento;
+	}
+
+	public void setDataPagamento(Date dataPagamento) {
+		this.dataPagamento = dataPagamento;
+	}
+
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
+
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+
+	
+}
