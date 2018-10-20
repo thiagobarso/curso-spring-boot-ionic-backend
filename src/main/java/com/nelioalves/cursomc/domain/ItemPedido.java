1475897b97700d5078a6858d4cf93b1ed6cd2d30
Thiago Barbosa Soares <thiagobarso@gmail.com>
2018-10-20 10:51:00 -0300

    video 050

--- src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
+++ src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
@@ -80,6 +80,14 @@ public class ItemPedido implements Serializable {
 	public Produto getProduto() {
 		return this.id.getProduto();
 	}
+	
+	public void setPedido(Pedido pedido) {
+		this.id.setPedido(pedido);
+	}
+	
+	public void setProduto(Produto produto) {
+		this.id.setProduto(produto);
+	}
 
 	@Override
 	public int hashCode() {
