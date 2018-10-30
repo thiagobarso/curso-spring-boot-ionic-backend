ae6224908349ebc3c4074dda54b6ba1d113a0cb6
Thiago Barbosa Soares <thiagobarso@gmail.com>
2018-10-29 21:45:43 -0300

    aula 60

--- src/main/java/com/nelioalves/cursomc/config/TestConfig.java
+++ src/main/java/com/nelioalves/cursomc/config/TestConfig.java
@@ -8,20 +8,26 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.nelioalves.cursomc.services.DBService;
+import com.nelioalves.cursomc.services.EmailService;
+import com.nelioalves.cursomc.services.MockEmailService;
 
 @Configuration
 @Profile("test")
 public class TestConfig {
 
-	
 	@Autowired
 	private DBService dbService;
-	
+
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
-		
+
 		dbService.instantiateTestDatabase();
-		
+
 		return true;
+	}
+
+	@Bean
+	public EmailService emailService() {
+		return new MockEmailService();
 	}
 }
