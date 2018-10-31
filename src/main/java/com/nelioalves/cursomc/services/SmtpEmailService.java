a1e060bbe4dac69c0ab11ad05bfedb44e350f1ba
Thiago Barbosa Soares <thiagobarso@gmail.com>
2018-10-30 21:39:23 -0300

    video 063

--- src/main/java/com/nelioalves/cursomc/services/SmtpEmailService.java
+++ src/main/java/com/nelioalves/cursomc/services/SmtpEmailService.java
@@ -1,10 +1,13 @@
 package com.nelioalves.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 public class SmtpEmailService extends AbstractEmailService {
 
@@ -13,12 +16,22 @@ public class SmtpEmailService extends AbstractEmailService {
 	@Autowired
 	private MailSender mailSender;
 	
+	@Autowired
+	private JavaMailSender javaMailSender;
+	
 	@Override
 	public void sendEmail(SimpleMailMessage msg) {
 		LOG.info("Enviando envio de e-mail...");
 		mailSender.send(msg);
 		LOG.info("email enviado");
 		
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Enviando envio de e-mail...");
+		javaMailSender.send(msg);
+		LOG.info("email enviado");
 	}
 
 }
